3e74421131ff5a76a5d8ecf1145c183c400d2c20
Imanstr26 <isatriowicaksono@gmail.com>
2026-04-26 10:40:31 +0700

    update test case 03 and 04

--- src/main/java/com/p2p/service/LoanService.java
+++ src/main/java/com/p2p/service/LoanService.java
@@ -7,6 +7,7 @@ public class LoanService {
     public Loan createLoan(Borrower borrower, BigDecimal amount) {
 
         validateBorrower(borrower);
+        validateAmount(amount);
 
         Loan loan = new Loan();
         if (borrower.getCreditScore() >= 600) {
@@ -14,14 +15,20 @@ public class LoanService {
         } else {
             loan.reject();
         }
-
         return loan;
     }
 
+    // Untuk validasi KYC TC 01
     private void validateBorrower(Borrower borrower) {
         if (!borrower.canApplyLoan()) {
             throw new IllegalArgumentException("Borrower not verified");
         }
     }
 
+    // Validasi jumlah pinjaman agar lebih dari 0 TC 02
+    private void validateAmount(BigDecimal amount){
+        if (amount == null || amount.compareTo(BigDecimal.ZERO) <= 0){
+            throw new IllegalArgumentException("amount must be greater than zero");
+        }
+    } 
 }
--- src/test/java/com/p2p/service/LoanServiceTest.java
+++ src/test/java/com/p2p/service/LoanServiceTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 import java.math.BigDecimal;
 import com.p2p.domain.Borrower;
+import com.p2p.domain.Loan;
 
 public class LoanServiceTest {
     @Test
@@ -20,7 +21,7 @@ public class LoanServiceTest {
         // Arrange (Initial Condition)
         // =========================
         // Borrower belum lolos proses KYC
-        Borrower borrower = new Borrower(true, 700);
+        Borrower borrower = new Borrower(false, 700);
 
         // Service untuk pengajuan loan
         LoanService loanService = new LoanService();
@@ -28,15 +29,71 @@ public class LoanServiceTest {
         // Jumlah pinjaman valid
         BigDecimal amount = BigDecimal.valueOf(1000);
 
-        // =========================
-        // Act (Action)
-        // =========================
-        // Borrower mencoba mengajukan loan
-        loanService.createLoan(borrower, amount);
+        // Kita berharap (Assert) akan ada Exception yang dilempar
+        assertThrows(IllegalArgumentException.class, () -> {
+            // Di dalam sini adalah tindakan (Act) yang memicu error
+            loanService.createLoan(borrower, amount);
+        });
+    }
 
-        // =========================
-        // Assert (Expected Result)
-        // =========================
-        assertTrue(true);
+    @Test 
+    void shouldRejectLoanWhenAmountIsZeroOrNegative() { // test case 2
+        // SKENARIO 
+        // JANGAN SAMPAI ADA ORANG YANG BISA PINJAM 0 ATAU MINUS
+
+        // membuat objek peminjam di test case 2 dengan tingkat kepercayaan 700
+        Borrower borrower2 = new Borrower(false, 500);
+
+        // membuat objek pemberi pinjaman
+        LoanService service2 = new LoanService();
+
+        // jumlah pinjaman 
+        BigDecimal amount = BigDecimal.ZERO;
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            service2.createLoan(borrower2, amount);
+        });
+    }
+
+    @Test
+    void shouldApproveLoanWhenCreditScoreHigh(){
+        // SKENARIO
+        // memberikan persetujuan jika credit scorenya tinggi 
+
+        // membuat objek peminjam dengan tingkat kepercayaan >= 600
+        Borrower borrower3 = new Borrower(true, 800);
+
+        // membuat objek pemberi pinjaman
+        LoanService service3 = new LoanService();
+
+        // jumlah pinjaman yang diajukan
+        BigDecimal amount3 = BigDecimal.valueOf(400);
+
+        //memanggil methode dari objek peminjam
+        Loan HasilLoan = service3.createLoan(borrower3, amount3);
+
+        // memeriksa apakah status hasil loan di approve atau tidak
+        assertEquals(HasilLoan.getStatus(), Loan.Status.APPROVED);
+    }
+
+    @Test
+    void shouldRejectLoanWhenCreditScoreLow(){
+        // SKENARIO
+        // menolak saat credit score rendah 
+
+        // membuat objek peminjam dengan tingkat kepercayaan <= 600
+        Borrower borrower4 = new Borrower(false, 200);
+
+        // membuat objek pemberi pinjaman 
+        LoanService service4 = new LoanService();
+
+        // jumlah pinjaman
+        BigDecimal amount4 = BigDecimal.valueOf(1000);
+
+        // memanggil methode dari objem pemberi pinjaman
+        Loan Hasil = service4.createLoan(borrower4, amount4);
+
+        // memeriksa status 
+        assertEquals(Hasil.getStatus(), Loan.Status.REJECTED);
     }
 }
